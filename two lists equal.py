8985570b42f249c73007ba0f848fa3afb59eb052
ssingh13-lang <ssingh13@student.gitam.edu>
2026-03-04 22:41:08 +0530

    Create two lists equal.py

---
+++ two lists equal.py
@@ -0,0 +1,13 @@
+list1 = list(map(int, input().split()))
+list2 = list(map(int, input().split()))
+
+if list1 == list2:
+    print("Lists are equal")
+else:
+    print("Lists are not equal")
+
+#input
+1 2 3 4
+1 2 3 4
+#output
+Lists are equal
