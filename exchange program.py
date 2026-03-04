6f2ea9e37b03a5e9adc2ec3c4ee6470d8b08f99b
ssingh13-lang <ssingh13@student.gitam.edu>
2026-03-04 22:30:53 +0530

    Create exchange program.py

---
+++ exchange program.py
@@ -0,0 +1,15 @@
+m = int(input("Enter cookies with Shanmukesh: "))
+n = int(input("Enter cookies with Gurupreet: "))
+
+print("Cookies at the beginning:", m, n)
+
+m, n = n, m
+
+print("Cookies after 1st interchange:", m, n)
+
+#input
+19
+21
+#output
+Cookies at the beginning: 19 21
+Cookies after 1st interchange: 21 19
